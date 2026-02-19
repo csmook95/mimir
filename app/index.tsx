51e910e5a899e9c2c0d9ccecab22b31a60f3ee31
csmook95 <csmook95@gmail.com>
2026-02-19 17:45:42 +0900

    chore: ESLint 설정 추가 및 종속성 업데이트
    ESLint를 추가하고 코드 스타일 및 일관성을 위한 규칙을 구성했습니다.
    주요 변경 사항은 다음과 같습니다:
    
    *   eslint.config.js에 @stylistic 및 eslint-plugin-prefer-arrow-functions 플러그인 추가 및 규칙 설정.
    *   문자열에 백틱 사용(acktick)을 강제하고 JSX 포맷팅 규칙을 적용하여 코드 스타일 통일.
    *   un.lock 및 package.json의 종속성 패키지 버전 업데이트 (expo, expo-font, expo-router 등).
    *   pp/index.tsx에서 문자열 리터럴을 백틱으로 변경하여 스타일 규칙 적용.

--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,9 @@ export default function Index() {
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+        justifyContent: `center`,
+        alignItems: `center`,
+      }}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
